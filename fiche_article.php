0db2736fa9f33fec0d223dccea64d31af5516e33
celine-baudoin <celine.baudoin31@gmail.com>
2019-05-27 20:23:30 +0200

    27/05 20h23

--- fiche_article.php
+++ fiche_article.php
@@ -8,29 +8,42 @@ if(isset($_GET['Id_Produit']) AND !empty($_GET['Id_Produit']))
     $Id_Produit = htmlspecialchars($_GET['Id_Produit']);
     $article = $bdd->prepare('SELECT * FROM produits INNER JOIN types ON produits.Id_Type_Produit = types.Id_Type INNER JOIN membres ON produits.Id_Membre_Produit = membres.Id_Membre WHERE Produits.Id_Produit = ?');
     $article->execute(array($Id_Produit));
+    $Donnees_Produit = $article->fetch();
+    $Libelle_Produit = $Donnees_Produit['Libelle_Produit'];
+    $Description_Produit = $Donnees_Produit['Description_Produit'];
+    $Prix_Produit = $Donnees_Produit['Prix_Produit'];
+    $commentaires = $bdd->query('SELECT * FROM commentaires INNER JOIN membres ON commentaires.Id_Membre_Com = membres.Id_Membre INNER JOIN produits ON commentaires.Id_Produit_Com = produits.Id_Produit WHERE Produits.Id_Produit ='.$Id_Produit);
+    if ($commentaires !== false) {
+        $Donnees_Commentaire = $commentaires->fetchAll();    
+    }
+    
 
 
-    if($article->rowCount() == 1)
-    {
-        $article = $article->fetch();
-        $Libelle_Produit = $article['Libelle_Produit'];
-        $Description = $article['Description_Produit'];
-        $Prix = $article['Prix_Produit'];
-    }
-    else
-    {
-        die('Cet article n\'existe pas');
-    }
+    
 }
 else
 {
     die('Erreur');
 }
+if (isset($_POST['comment'])) {
+    if (!empty($_POST['objet']) && !empty($_POST['commentaire'])) {
+        $Objet_Com = htmlspecialchars($_POST['objet']);
+        $Texte_Com = htmlspecialchars($_POST['commentaire']);
+        $Date_Com = date("Y-m-d H:i:s");
+        $Id_Produit_Com = $Id_Produit;
+        $Id_Membre_Com = intval($_SESSION['Id_Membre']);
+        try {
+            $req = $bdd->prepare('INSERT INTO commentaires(Objet_Com, Date_Com, Texte_Com, Id_Produit_Com, Id_Membre_Com) VALUES (?,?,?,?,?)');
+            $req->execute(array($Objet_Com,$Date_Com,$Texte_Com,$Id_Produit_Com,$Id_Membre_Com));
+            echo "<p>Votre commentaire a bien été posté</p>";
+        } catch (\Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+}
 
 
 
-$commentaires = $bdd->query('SELECT * FROM commentaires INNER JOIN membres ON commentaires.Id_Membre_Com = membres.Id_Membre INNER JOIN produits ON commentaires.Id_Produit_Com = produits.Id_Produit WHERE Produits.Id_Produit ='.$Id_Produit);
-$donnees = $commentaires->fetchAll();
 ?>
 
 
@@ -47,38 +60,26 @@ $donnees = $commentaires->fetchAll();
   <div id="ensemble">
     <div id="ensembleFicheProduit">
       <h1><?= $Libelle_Produit ?></h1>
-      <img src="<?php echo $article['Image_Produit'] ?>" alt="Image produit"/>
-      <p><?= $Description ?></p>
-      <p><?= $Prix ?>€
-              <?php
-         if(isset($_SESSION['Id_Membre'])) {
-        ?><form action="fiche_article.php?Id_Produit=<?= $article['Id_Produit'] ?>" method="POST"><input type="submit" name="ajout_panier" value="Ajouter au panier"></form>
-        <?php
-         } else {
-             ?>
-              <p>Vous devez d'abord vous connecter pour pouvoir vendre vos articles. <a href="connexion.php">Connexion</a></p>
-              <p>Pas encore de compte ? <a href="inscription.php">Inscrivez-vous !</a></p>
-              <?php
-         }
-    ?>
+      <img src="<?php echo $Donnees_Produit['Image_Produit'] ?>" alt="Image produit"/>
+      <p><?= $Description_Produit ?></p>
+      <p><?= $Prix_Produit ?>€
+        <form action="fiche_article.php?Id_Produit=<?=$Donnees_Produit['Id_Produit'] ?>" method="POST"><input type="submit" name="ajout_panier" value="Ajouter au panier"></form>
         <?php
         if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['ajout_panier'])) {
-          ajouterArticle($Id_Produit,$Libelle_Produit,$Prix);
+          ajouterArticle($Id_Produit,$Libelle_Produit,$Prix_Produit);
         }
         ?>
       </p>
-      <?php
- if(isset($_SESSION['Id_membre'])) {
-?>
+
       <div class="commentaire">
         <h2>Posez une question au vendeur</h2>
-        <form class="" action="fiche_article.php" method="post">
+        <form class="" action="fiche_article.php?Id_Produit=<?=$Donnees_Produit['Id_Produit'];?>" method="post">
           <input type="text" name="objet" placeholder="Objet">
           <input type="text" name="commentaire" placeholder="Commentaire"/>
-          <input type="submit" value="Envoyer"/>
+          <input type="submit" value="Envoyer" name="comment"/>
         </form>
         <?php
-          foreach ($donnees as $row) {
+          foreach ($Donnees_Commentaire as $row) {
             if ($row['Id_Membre_Com'] == $row['Id_Membre_Produit']) { ?>
             <div class="reponse">
               <h3 class="objet"><?php echo $row['Objet_Com'] ?></h3>
@@ -94,8 +95,6 @@ $donnees = $commentaires->fetchAll();
           <?php }
         } ?>
       </div>
-          <?php 
-        } ?>
     </div>
   </div>
 
